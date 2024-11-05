20dabc8447233f85efafe9a19573f1caf19f6327
Tatiana Trofimova <nerpassevera@gmail.com>
2024-11-05 14:20:12 -0800

    Adds create_task to task_routes.py

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -4,3 +4,21 @@ from app.db import db
 
 
 bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks/")
+
+@bp.post("")
+def create_task():
+    req_body = request.get_json()
+    title = req_body["title"]
+    description = req_body["description"]
+    completed_at = req_body.get("completed_at", None)
+
+    new_task = Task(
+            title=title,
+            description=description,
+            completed_at=completed_at,
+    )
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return {"task": new_task.to_dict()}, 201
