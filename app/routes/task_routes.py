8ad364ab9d93aae5eda7df482776a1a223de6c9f
Tatiana Trofimova <nerpassevera@gmail.com>
2024-11-05 14:14:03 -0800

    Adds Task blueprint

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from app.models.task import Task
+from app.db import db
+
+
+bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks/")
