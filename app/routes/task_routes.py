1a41d6073f757f3db52cd7c40bda99197495fe87
Tatiana Trofimova <nerpassevera@gmail.com>
2024-11-05 14:58:27 -0800

    Adds get_all_tasks function

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -23,6 +23,27 @@ def create_task():
 
     return {"task": new_task.to_dict()}, 201
 @bp.get("/<task_id>")
+@bp.get("/", strict_slashes=False)
+def get_all_tasks():
+    query = db.select(Task).order_by(Task.id)
+
+    for attribute, value in request.args.items():
+        if hasattr(Task, attribute):
+            query = query.where(getattr(Task, attribute).ilike(f"%{value}%"))
+
+    tasks = db.session.scalars(query)
+    return [task.to_dict() for task in tasks], 200
+
+@bp.get("/<task_id>")
+def get_one_task(task_id):
+    try:
+        task_id = int(task_id)
+    except ValueError:
+        return {}
+    query = db.select(Task).where(Task.id == task_id)
+    task = db.session.scalar(query)
+
+    return { "task": task.to_dict() }, 200
 def get_one_task(task_id):
     try:
         task_id = int(task_id)
