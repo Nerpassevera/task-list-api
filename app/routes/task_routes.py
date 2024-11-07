3c6bb1f44b6acb93b6cd1135a0905f75cf2d2da3
Tatiana Trofimova <nerpassevera@gmail.com>
2024-11-07 13:21:01 -0800

    Adds message sending functionality to mark_task_completed

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, request, make_response, abort
 from datetime import datetime
+import requests
 from app.models.task import Task
 from app.db import db
+from os import environ
 
 
 bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
@@ -82,7 +84,12 @@ def mark_task_completed(task_id):
     task = validate_task(task_id)
     task.completed_at = datetime.now()
     db.session.commit()
-
+    message_is_sent = send_task_complete_message(task.title)
+    print(message_is_sent)
+    if not message_is_sent:
+        raise Exception(
+            "An error occured during notification sending!\
+                Please connect to the Task List developer!")
     return { "task": task.to_dict() }, 200
 
 @bp.patch("/<task_id>/mark_incomplete")
@@ -92,3 +99,19 @@ def mark_task_incompleted(task_id):
     db.session.commit()
 
     return { "task": task.to_dict() }, 200
+
+def send_task_complete_message(task_title):
+    request_data = {
+    # "channel": "#api-test-channel", # Slack channel for tests
+    "channel": "U07GC9C8Y4X", # My Slack account ID
+    "username": "Task List app",
+    "text": f"Someone just completed the task \"{task_title}\""
+    }
+    message_status = requests.post(
+        url="https://slack.com/api/chat.postMessage", 
+        json=request_data,
+        headers={"Authorization": environ.get('SLACK_API_KEY')},
+        timeout=1
+    )
+
+    return message_status.json()["ok"]
