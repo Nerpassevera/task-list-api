721fe9413175e5196954c8872a04aa4d93d1480a
Tatiana Trofimova <nerpassevera@gmail.com>
2024-12-02 11:41:43 -0800

    Fixing the code

--- app/routes/route_utilities.py
+++ app/routes/route_utilities.py
@@ -6,21 +6,21 @@ def apply_filters(cls, arguments, query):
         if hasattr(cls, attribute):
             query = query.where(getattr(cls, attribute).ilike(f"%{value}%"))
 
-# def validate_model(cls, cls_id):
-#     try:
-#         cls_id = int(cls_id)
-#     except ValueError:
-#         message = { "message": f"{cls.__name__} ID {cls_id} is invalid"}
-#         abort(make_response(message, 400))
+def validate_model(cls, cls_id):
+    try:
+        cls_id = int(cls_id)
+    except ValueError:
+        message = { "message": f"{cls.__name__} ID {cls_id} is invalid"}
+        abort(make_response(message, 400))
 
-#     query = db.select(cls).where(cls.id == cls_id)
-#     result = db.session.scalar(query)
+    query = db.select(cls).where(cls.id == cls_id)
+    result = db.session.scalar(query)
 
-#     if not result:
-#         message = {"message": f"{cls.__name__} with ID {cls_id} was not found"}
-#         abort(make_response(message, 404))
+    if not result:
+        message = {"message": f"{cls.__name__} with ID {cls_id} was not found"}
+        abort(make_response(message, 404))
 
-#     return result
+    return result
 
 def set_new_attributes(instance, req_body):
     for attr, value in req_body.items():
